9fa6e6c1b3cb64101d5616b8d0674a37367520b6
John Eatwell <jceatwell@gmail.com>
2021-03-10 23:13:39 +0200

    Add Basic Routing and Testing
    Following updated:
    - Added BrowserRouter into index.js
    - Added Switch Route into App (Note Unit Tests need to take this into
      account
    - MenuItem uses HigherOrderComponent (withRouter) which allows history,
      match and location access without going through props drilling

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Tests', () => {
+
+  test('renders HATS react link', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const linkElement = screen.getByText(/HATS/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('Another way of setting the same things', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const linkElement = getByText(/HATS/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      , div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
 });
+
